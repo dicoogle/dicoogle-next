aebb290d28c2e947b5144ba5433d409bed8a819e
luis-godinho <125367612+luis-godinho@users.noreply.github.com>
2026-01-26 15:20:43 +0000

    Fix repository structure

--- src/types/cornerstone-dicom-image-loader.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "@cornerstonejs/dicom-image-loader" {
-  const cornerstoneDICOMImageLoader: any;
-  export default cornerstoneDICOMImageLoader;
-  export const external: any;
-  export const webWorkerManager: any;
-  export function configure(config: any): void;
-}
