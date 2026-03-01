076cd404bfbec5b7cfef4a17cfa018849cdcaa9a
luis-godinho <125367612+luis-godinho@users.noreply.github.com>
2026-03-01 15:41:57 +0000

    Filesystem manager Integration
    * feat: added a filesystem manager and backend end to expose it
    
    * fix: updated vulnerable packages. chore: added CORS headers to filesystem manager
    
    * chore: changed docker compose config to expose test directory
    
    * fix: linting errors

---
+++ webapp/src/types/chonky.d.ts
@@ -0,0 +1,13 @@
+
+declare module 'chonky' {
+  export const FileBrowser: any;
+  export const FileNavbar: any;
+  export const FileToolbar: any;
+  export const FileList: any;
+  export const FileContextMenu: any;
+  export const ChonkyActions: any;
+  export const setChonkyDefaults: (config: any) => void;
+
+  export type ChonkyFileActionData = any;
+  export type FileData = any;
+}
