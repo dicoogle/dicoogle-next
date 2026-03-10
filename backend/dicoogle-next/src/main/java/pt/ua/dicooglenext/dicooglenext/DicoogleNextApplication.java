e2a8bd6c50025779e4812545aaea430af887efba
luis-godinho <125367612+luis-godinho@users.noreply.github.com>
2026-03-10 16:27:42 +0000

    feat: maven project initial setup

---
+++ backend/dicoogle-next/src/main/java/pt/ua/dicooglenext/dicooglenext/DicoogleNextApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.dicooglenext.dicooglenext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoogleNextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicoogleNextApplication.class, args);
+    }
+
+}
---
+++ backend/dicoogle-next/src/test/java/pt/ua/dicooglenext/dicooglenext/DicoogleNextApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.dicooglenext.dicooglenext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicoogleNextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
