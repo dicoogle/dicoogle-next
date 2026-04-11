b6727e4057a231d995c0c5e1e5ff3140c5fab100
luis-godinho <125367612+luis-godinho@users.noreply.github.com>
2026-04-11 15:44:46 +0100

    test: cover C-FIND query task cancel signaling

--- backend/dicoogle-next-protocol-dimse/src/main/java/org/dicoogle/protocol/dimse/DimseCFindSCP.java
+++ backend/dicoogle-next-protocol-dimse/src/main/java/org/dicoogle/protocol/dimse/DimseCFindSCP.java
@@ -1,13 +1,10 @@
 package org.dicoogle.protocol.dimse;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.net.Association;
 import org.dcm4che3.net.pdu.PresentationContext;
 import org.dcm4che3.net.service.BasicCFindSCP;
-import org.dcm4che3.net.service.BasicQueryTask;
 import org.dcm4che3.net.service.DicomServiceException;
 import org.dcm4che3.net.service.QueryTask;
 import org.slf4j.Logger;
@@ -41,30 +38,6 @@ class DimseCFindSCP extends BasicCFindSCP {
             as.getCallingAET(),
             as.getCalledAET(),
             as.getSerialNo());
-    return new ListQueryTask(as, pc, rq, keys, matches);
-  }
-
-  private static final class ListQueryTask extends BasicQueryTask {
-    private final Iterator<Attributes> iterator;
-
-    private ListQueryTask(
-        Association as,
-        PresentationContext pc,
-        Attributes rq,
-        Attributes keys,
-        List<Attributes> matches) {
-      super(as, pc, rq, keys);
-      this.iterator = new ArrayList<>(matches).iterator();
-    }
-
-    @Override
-    protected boolean hasMoreMatches() {
-      return iterator.hasNext();
-    }
-
-    @Override
-    protected Attributes nextMatch() {
-      return iterator.next();
-    }
+    return new DimseListQueryTask(as, pc, rq, keys, matches);
   }
 }
